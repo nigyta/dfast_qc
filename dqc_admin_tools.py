5df626ca36df0128bd83034483288201f0377fa9
Mohamed Elmanzalawi <mohammed.elmanzalawi@gmail.com>
2024-04-26 18:22:34 +0900

    "Removed FastANI related functions, arguments, and files"

--- dqc_admin_tools.py
+++ dqc_admin_tools.py
@@ -44,14 +44,6 @@ def mash_gtdb_sketch(args):
     from dqc.admin.mash_gtdb_sketching import gtdb_sketching
     gtdb_sketching()
 
-def prepare_reference_hmm(args):
-    from dqc.admin.prepare_reference_hmm import prepare_reference_hmm
-    prepare_reference_hmm()
-
-def prepare_reference_marker_fasta(args):
-    from dqc.admin.prepare_reference_marker_fasta import prepare_reference_marker_fasta
-    prepare_reference_marker_fasta(delete_existing=args.delete_existing_marker, for_gtdb=args.for_gtdb)
-
 def prepare_sqlite_db(args):
     from dqc.admin.prepare_sqlite_db import prepare_sqlite_db, prepare_sqlite_db_for_gtdb
     if args.for_gtdb:
@@ -101,9 +93,9 @@ def parse_args():
     parser_master = subparsers.add_parser('download_master_files', help='Download master files.', parents=[common_parser])
     parser_master.add_argument(
         "--targets", type=str, required=False, metavar="STR", 
-        choices=['asm', 'ani', 'tsr', "igp", "sst", "hmm", "checkm", "taxdump", "gtdb"], nargs="*",
+        choices=['asm', 'ani', 'tsr', "igp", "sst", "checkm", "taxdump", "gtdb"], nargs="*",
         help="Target(s) for downloading. " + 
-             "[asm: Assembly report, ani: ANI report, tsr: Type strain report, hmm: TIGR HMMER profile, igp: indistinguishable groups prokaryotes, sst: ANI species specific threshold, checkm: CheckM reference data, taxdump: NCBI taxdump.tar.gz, gtdb: GTDB representative species list] "
+             "[asm: Assembly report, ani: ANI report, tsr: Type strain report,igp: indistinguishable groups prokaryotes, sst: ANI species specific threshold, checkm: CheckM reference data, taxdump: NCBI taxdump.tar.gz, gtdb: GTDB representative species list] "
              "(default: asm ani tsr igp)"
     )
     parser_master.set_defaults(func=download_master_files)
@@ -124,20 +116,6 @@ def parse_args():
     parser_sketch_gtdb = subparsers.add_parser('mash_gtdb_sketch', help='Sketch the GTDB genomes.', parents=[common_parser])
     parser_sketch_gtdb.set_defaults(func=mash_gtdb_sketch)
     
-    # subparser for prepare_reference_hmm
-    parser_prep_ref_hmm = subparsers.add_parser('prepare_reference_hmm', help='Prepare reference profile HMM file　(reference_markers.hmm).', parents=[common_parser])
-    # parser_prep_ref_hmm.add_argument(
-    #     "--master_hmm_file", type=str, required=False, metavar="PATH", 
-    #     help="Path to profile HMM for TIGRFAMs_15 (raw or gzipped). (Default: auto download)"
-    # )
-    parser_prep_ref_hmm.set_defaults(func=prepare_reference_hmm)
-
-    # subparser for prepare_reference_fasta
-    parser_prep_ref_fasta = subparsers.add_parser('prepare_reference_fasta', help='Prepare reference marker FASTA file (reference_markers.fasta).', parents=[common_parser])
-    parser_prep_ref_fasta.add_argument('--delete_existing_marker', action='store_true', help='Delete existing markers and recreate all markers.')
-    parser_prep_ref_fasta.add_argument('--for_gtdb', action='store_true', help='Create files for GTDB.')
-    parser_prep_ref_fasta.set_defaults(func=prepare_reference_marker_fasta)
-
     # subparser for prepare sqlite DB
     parser_prep_sqlite = subparsers.add_parser('prepare_sqlite_db', help='Prepare SQLite database (references.db).', parents=[common_parser])
     parser_prep_sqlite.add_argument('--for_gtdb', action='store_true', help='Create files for GTDB.')
